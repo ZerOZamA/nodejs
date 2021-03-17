f17b03d7e181f4646b4c5e842f8409cd5e8766ba
Israel Ladron De Guevara Domingo <Israel.Domingo@ibm.com>
2021-03-17 12:16:03 -0600

    push homework 5

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # nodejs
 taller node js 
 Dev/null Before dishonor
+
+## taller node js ··
+# autor israel ladron de guevara domingo
--- index.js
+++ index.js
@@ -1,6 +1,57 @@
 const express = require('express');
 const app =express();
+const {pokemon}= require('./pokedex.json');
 app.get("/",(req,res,next)=>{
+    
+    var myImages = '';
+    for(var i = 0; i < pokemon.length; i++){
+        myImages += "<p>"+pokemon[i]['name'] +"</p>"+'<img src="'+pokemon[i]['img']+'" />';
+        }
+      
     res.status(200);
-    res.send("hello class ");});
+    res.send( "Pokedex nacional kanto"+myImages);});
+
+app.get("/landing/pokedex",(req,res,next)=>{
+
+        res.status(200);
+        res.send(pokemon)
+        
+        });
+
+app.get("/landing/pokedex/:id([0-9]{1,3})",(req,res,next)=>{
+    id=req.params.id-1;
+    var pkid= pokemon[id];
+    if (id <= 150){
+        var html = "<div><p>"+pkid['name']+"</p>"+'<img src="'+pkid["img"]+'"/>'+"<p>"+pkid['type']+"</p></div>";
+        res.status(200);
+        res.send(html);
+    }
+    else{
+
+        res.status(404);
+        res.send("pokemon missing")
+    }
+
+});
+
+app.get("/landing/pokedex/:name",(req,res,next)=>{
+    name1=req.params.name;
+    name1=name1.toLowerCase()
+    pokename=""
+    for(var i = 0; i < pokemon.length; i++){
+        pokename=pokemon[i]['name']
+        if (pokename.toLowerCase()==name1){
+
+            res.status(200);
+
+            res.send(pokemon[i])
+
+        }
+        
+        }
+        res.status(404);
+        res.send("pokemon missing")
+});
+
+
 app.listen(3000, ()=>{console.log('server running')});
