9224b33d51d33466fcd9d973c38627b01084c028
Israel Ladron De Guevara Domingo <Israel.Domingo@ibm.com>
2021-03-18 17:09:15 -0600

    comit to your master

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app =express();
 const {pokemon}= require('./pokedex.json');
+const bodyParser= require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
 app.get("/",(req,res,next)=>{
     
     var myImages = '';
@@ -8,50 +13,49 @@ app.get("/",(req,res,next)=>{
         myImages += "<p>"+pokemon[i]['name'] +"</p>"+'<img src="'+pokemon[i]['img']+'" />';
         }
       
-    res.status(200);
-    res.send( "Pokedex nacional kanto"+myImages);});
+    return res.status(200).send( "Pokedex nacional kanto"+myImages);});
+
+
+app.post("/pokemon", (req,res , next)=>{
+
+    return res.status(200).send(req.body)
+
+}
+);
 
 app.get("/landing/pokedex",(req,res,next)=>{
 
-        res.status(200);
-        res.send(pokemon)
+        return res.status(200).send(pokemon)
         
         });
 
 app.get("/landing/pokedex/:id([0-9]{1,3})",(req,res,next)=>{
-    id=req.params.id-1;
+    var id=req.params.id-1;
     var pkid= pokemon[id];
     if (id <= 150){
         var html = "<div><p>"+pkid['name']+"</p>"+'<img src="'+pkid["img"]+'"/>'+"<p>"+pkid['type']+"</p></div>";
-        res.status(200);
-        res.send(html);
+        return res.status(200).send(html);
     }
     else{
 
-        res.status(404);
-        res.send("pokemon missing")
+        return res.status(404).send("pokemon missing");
     }
 
 });
 
-app.get("/landing/pokedex/:name",(req,res,next)=>{
-    name1=req.params.name;
-    name1=name1.toLowerCase()
-    pokename=""
-    for(var i = 0; i < pokemon.length; i++){
-        pokename=pokemon[i]['name']
-        if (pokename.toLowerCase()==name1){
-
-            res.status(200);
-
-            res.send(pokemon[i])
-
-        }
+app.get("/landing/pokedex/:name([A-Za-z]+)",(req,res,next)=>{
+    var name1=req.params.name;
+    var name1=name1.toLowerCase();
+    console.log(name1);
+    var pokename= pokemon.filter((poki) => {
+        return (poki.name.toLowerCase() == name1) && poki;
         
-        }
-        res.status(404);
-        res.send("pokemon missing")
+
+    });
+   ( pokename.length > 0 )?
+    res.status(200).send(pokename) :
+    res.status(404).send("not found");
 });
 
 
-app.listen(3000, ()=>{console.log('server running')});
+app.listen(process.env.PORT || 3000 , ()=>{console.log('server running')});
