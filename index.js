f45ba101e72d9eea8fb50df910d0c9dc48f34897
ZerOZamA <necrofagoo@hotmail.com>
2021-03-23 22:03:26 -0600

    owo

--- index.js
+++ index.js
@@ -1,61 +1,17 @@
 const express = require('express');
 const app =express();
-const {pokemon}= require('./pokedex.json');
 const bodyParser= require('body-parser');
+const morgan = require('morgan');
+const pokemon = require('./routes/pokemon');
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
-
 app.get("/",(req,res,next)=>{
-    
-    var myImages = '';
-    for(var i = 0; i < pokemon.length; i++){
-        myImages += "<p>"+pokemon[i]['name'] +"</p>"+'<img src="'+pokemon[i]['img']+'" />';
-        }
-      
-    return res.status(200).send( "Pokedex nacional kanto"+myImages);});
 
+return res.status(200).send( "Pokedex nacional kanto");});
 
-app.post("/pokemon", (req,res , next)=>{
-
-    return res.status(200).send(req.body)
-
-}
-);
-
-app.get("/landing/pokedex",(req,res,next)=>{
-
-        return res.status(200).send(pokemon)
-        
-        });
-
-app.get("/landing/pokedex/:id([0-9]{1,3})",(req,res,next)=>{
-    var id=req.params.id-1;
-    var pkid= pokemon[id];
-    if (id <= 150){
-        var html = "<div><p>"+pkid['name']+"</p>"+'<img src="'+pkid["img"]+'"/>'+"<p>"+pkid['type']+"</p></div>";
-        return res.status(200).send(html);
-    }
-    else{
-
-        return res.status(404).send("pokemon missing");
-    }
-
-});
-
-app.get("/landing/pokedex/:name([A-Za-z]+)",(req,res,next)=>{
-    var name1=req.params.name;
-    var name1=name1.toLowerCase();
-    console.log(name1);
-    var pokename= pokemon.filter((poki) => {
-        return (poki.name.toLowerCase() == name1) && poki;
-        
-
-    });
-   ( pokename.length > 0 )?
-    res.status(200).send(pokename) :
-    res.status(404).send("not found");
-});
+app.use("/pokemon",pokemon);
 
 
 app.listen(process.env.PORT || 3000 , ()=>{console.log('server running')});
---
+++ routes/pokemon.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const pokemon =express.Router();
+/*const poop = require('../pokedex.json').pokemon;*/
+const db =require("../config/database");
+
+pokemon.post("/", (req,res , next)=>{
+    console.log("paso")
+    return res.status(200).send(req.body);
+
+}
+);
+
+pokemon.get("/",async (req,res,next)=>{
+    const pokemn = await db.query("SELECT * FROM pokemon");
+
+        return res.status(200).json(pokemn)
+        
+        }); 
+
+pokemon.get("/:id([0-9]{1,3})",async (req,res,next)=>{
+    const pokemn = await db.query("SELECT * FROM pokemon");
+
+    var id=req.params.id-1;
+    var pkid= pokemn[id];
+    if (id <= 722){
+        
+        return res.status(200).json(pkid);
+    }
+    else{
+
+        return res.status(404).send("pokemon missing");
+    }
+
+});
+
+pokemon.get("/:name([A-Za-z]+)",async(req,res,next)=>{
+
+    const pokemn = await db.query("SELECT * FROM pokemon");
+    var name1=req.params.name;
+    var name1=name1.toLowerCase();
+    console.log(name1);
+    var pokename= pokemn.filter((poki) => {
+        return (poki.pok_name.toLowerCase() == name1) && poki;
+        
+
+    });
+   ( pokename.length > 0 )? 
+    res.status(200).send(pokename) :
+    res.status(404).send("not found");
+});
+
+module.exports=pokemon;
