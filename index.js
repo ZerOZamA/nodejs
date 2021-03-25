1bf619dd6a7bf295e7c91600861ae3229bb2f190
ZerOZamA <necrofagoo@hotmail.com>
2021-03-25 16:39:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app =express();
-const bodyParser= require('body-parser');
 const morgan = require('morgan');
 const pokemon = require('./routes/pokemon');
 
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.json()):
+app.use(express.urlencoded({extended : true}));
 app.get("/",(req,res,next)=>{
 
 return res.status(200).send( "Pokedex nacional kanto");});
