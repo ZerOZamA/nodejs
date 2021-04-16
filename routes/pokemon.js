980a0e94bcfbd9dd40506887b8859609f607a176
ZerOZamA <necrofagoo@hotmail.com>
2021-04-15 20:41:39 -0500

    Update pokemon.js

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -18,7 +18,49 @@ pokemon.post("/",async (req,res , next)=>{
     return res.status(500).json({code:500, message:"error datos incompletos "});
 }   
 );
- 
+pokemon.delete("/:id([0-9]{1,3})",async(req,res,next)=>{
+    const query=`DELETE FROM pokemon where pok_id=${req.params.id}`;
+    const rows = await db.query(query);
+    if (rows.affectedRows == 1 ){
+        return res.status(200).json({code : 200, mensage : "operation effectuated"})
+
+    }
+    return res.status(400).json({code : 400, mensage : "operation not found"})
+});
+
+pokemon.put("/:id([0-9]{1,3})",async(req,res,next)=>{ 
+    const { pok_name, pok_height, pok_weight, pok_base_experience}=req.body;
+    if (pok_name && pok_height && pok_weight && pok_base_experience){
+        let query = `UPDATE pokemon SET pok_name='${pok_name}',pok_height=${pok_height},`;
+        query += ` pok_weight=${pok_weight}, pok_base_experience=${pok_base_experience} WHERE pok_id=${req.params.id}`;
+        const rows = await db.query(query);
+        console.log(query);
+        if (rows.affectedRows==1){
+            return res.status(201).json({code:201, message : " pokemon updated "});
+        }
+        return res.status(505).json({code:505, message:"error pokemon not found "});
+    }       
+    return res.status(501).json({code:501, message:"error datos incompletos "});
+}   
+    
+
+);
+pokemon.patch("/:id([0-9]{1,3})",async(req,res,next)=>{ 
+    
+    const { pok_name}=req.body;
+    if (pok_name){    
+        let query = `UPDATE pokemon SET pok_name='${pok_name}' WHERE pok_id=${req.params.id}`;
+        const rows = await db.query(query);
+        if (rows.affectedRows==1){
+            return res.status(201).json({code:201, message : " pokemon updated "});
+            }
+        return res.status(505).json({code:505, message:"error pokemon not found "});
+    } 
+    return res.status(505).json({code:505, message:"data incomplete "});
+}   
+    
+
+);
 pokemon.get("/",async (req,res,next)=>{
     const pokemn = await db.query("SELECT * FROM pokemon");
 
