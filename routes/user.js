42a69df96d9056001ac16f5fb566725c0343af67
ZerOZamA <necrofagoo@hotmail.com>
2021-04-22 18:18:54 -0500

    owo

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const user =express.Router();
 /*const poop = require('../pokedex.json').pokemon;*/
 const db =require("../config/database");
 const pokemon = require('./pokemon');
+const jwt = require('jsonwebtoken');
 
 user.post("/",async(req,res,next)=>{
     const{user_name, user_mail, user_password}=req.body
@@ -21,9 +22,33 @@ return res.status(501).json({code:501, message:"error datos incompletos "});
 });
 
 user.get("/",async(req,res,next)=>{
+
     const query= "SELECT * FROM user"
     const rows = await db.query(query);
     return res.status(201).json({code:201, message : rows});
 
+});
+
+user.post("/login",async(req,res,next)=>{
+    const {user_mail,user_password}= req.body;
+    const query = `SELECT * FROM user WHERE user_mail ='${user_mail}' AND user_password ='${user_password}';`;
+
+    if(user_mail && user_password){    
+    const rows= await db.query(query);
+    console.log(rows)
+    if (rows.length==1){
+        const token = jwt.sign({
+            user_id:rows[0].user_id,
+            user_mail:rows[0].user_mail
+        },"debugkey");
+        console.log(token)
+        return res.status(201).json({code:201, message :  token });
+
+    }
+    else {
+        return res.status(401).json({code:401, message:"error datos erroneos"});
+    }
+    
+}
 });
 module.exports=user;
