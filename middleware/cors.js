a2909c1c1bc8558f618f0275c76477584231a9af
ZerOZamA <necrofagoo@hotmail.com>
2021-05-11 18:04:07 -0500

    k

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,11 +1,11 @@
-(req,res,next)=>{
+module.exports=(req,res,next)=>{
     res.header("Acces-Control-Allow-Origin",'*');
     res.header(
-        "Acces-Control-Allow-Header",
+        'Access-Control-Allow-Headers',
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     if(req.method=='OPTIONS'){
-        res.header("Acces-Control-Allowed-Methods","PUT,PATCH,DELETE,POST,GET");
+        res.header("Access-Control-Allow-Methods", 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         return res.status(200).json({});
     };
 }
